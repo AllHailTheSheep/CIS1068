5c9a55076e49401da7eef7edcf99e35396fc6ec3
AllHailTheSheep <benfasick@gmail.com>
2022-02-18 23:42:23 -0500

    finish lab6

--- utils.java
+++ utils.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class utils {
     static final boolean DEBUG = true;
 
+
+    // begin input functions
     public static String get_input(String prompt, Scanner in) {
         /**
         * This function returns a string after prompting the user
         * if the input is not understood the function will re-run
         * @param prompt the promps to be presented to the user
+        * @param in the pre-initialized scanner to read input from
         * @return the string the user inputed
         */
         System.out.print(prompt);
@@ -30,6 +33,7 @@ public class utils {
         * This function returns an int after prompting the user
         * if the input is not an int the function will re-run
         * @param prompt to be presented to user
+        * @param in the pre-initialized scanner to read input from
         * @return the input the user entered as an int
         */
         System.out.print(prompt);
@@ -52,6 +56,7 @@ public class utils {
         * This function returns a double after prompting the user
         * if the input is not a double the function will re-run
         * @param prompt the promps to be presented to user
+        * @param in the pre-initialized scanner to read input from
         * @return the input the user entered as a double
         */
         System.out.print(prompt);
@@ -67,5 +72,14 @@ public class utils {
             result = get_double_input(prompt, in);
         }
         return result;
+    }
+
+    // begin string functions
+    public static String capitalize(String str) {
+        /** This function return the string passed in with the first character capitalized
+        * @param str the string to capitalize
+        * @return the capitalized string
+        */
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 }
