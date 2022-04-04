15508ed474e3e2668778613690476b2464de2843
AllHailTheSheep <benfasick@gmail.com>
2022-04-04 15:37:12 -0400

    refactoring

---
+++ lab7/lab7tests.java
@@ -0,0 +1,22 @@
+package lab7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class lab7tests extends lab7 {
+    @Test
+    void testSouthielyzer() {
+        assertEquals(southiealyze("I left my car keys by the harbor."), "I left my cah keys by the hahboh.");
+        assertEquals(southiealyze("a tuna"), "a tunar");
+        assertEquals(southiealyze("Cuba"), "Cubar");
+        assertEquals(southiealyze("idea."), "idear.");
+        assertEquals(southiealyze("very hard"), "wicked hahd");
+        assertEquals(southiealyze("deer"), "deeyah");
+        assertEquals(southiealyze("veneers"), "veneehs");
+        assertEquals(southiealyze("door"), "doowah");
+        assertEquals(southiealyze("doors"), "doohs");
+    }
+    
+}
